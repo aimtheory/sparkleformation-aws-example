bfdf3ba5c3bb2e594902f2e6fc5aa062866009ed
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-01 14:40:14 -0500

    Adding security group and port 22 access for working, connectable example

--- cloudformation/templates/website_elb_doc_example.rb
+++ cloudformation/templates/website_elb_doc_example.rb
@@ -21,6 +21,21 @@ SparkleFormation.new('website') do
     )
   end
 
+  resources.security_group_website do
+    type 'AWS::EC2::SecurityGroup'
+    properties do
+      group_description 'Enable SSH'
+      security_group_ingress array!(
+        -> {
+          ip_protocol 'tcp'
+          from_port 22
+          to_port 22
+          cidr_ip '0.0.0.0/0'
+        }
+      )
+    end
+  end
+
   resources.cfn_keys do
     type 'AWS::IAM::AccessKey'
     properties.user_name ref!(:cfn_user)
@@ -45,6 +60,7 @@ SparkleFormation.new('website') do
   resources.website_launch_config do
     type 'AWS::AutoScaling::LaunchConfiguration'
     properties do
+      security_groups [ ref!(:security_group_website) ]
       key_name 'sparkleinfrakey'
       image_id 'ami-59a4a230'
       instance_type 'm3.medium'
