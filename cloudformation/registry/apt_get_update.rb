f8e447604e2bc9b28891c58a930412d0dafc4b0b
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-02 16:59:45 -0500

    Added an example registry

---
+++ cloudformation/registry/apt_get_update.rb
@@ -0,0 +1,12 @@
+SparkleFormation::Registry.register(:apt_get_update) do
+  metadata('AWS::CloudFormation::Init') do
+    _camel_keys_set(:auto_disable) do
+      commands('01_apt_get_update') do
+        command 'sudo apt-get update'
+      end
+      commands('02_apt_get_upgrade') do
+        command 'sudo apt-get upgrade'
+      end
+    end
+  end
+end
