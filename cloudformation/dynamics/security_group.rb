18aa5a651187e81b472f0d1d75eebccd0be2160c
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-03 12:02:15 -0500

    Syntax error

--- cloudformation/dynamics/security_group.rb
+++ cloudformation/dynamics/security_group.rb
@@ -1,5 +1,9 @@
 SparkleFormation.dynamic(:security_group) do |_name, _config={}|
-  resources.("#{_name}_security_group".to_sym) do
+  resources("#{_name}_security_group".to_sym) do
     type 'AWS::EC2::SecurityGroup'
     properties do
-      
+      group_description "Security group for #{_name}"
+      security_group_ingress _config[:security_group_ingress] || []
+    end
+  end
+end
