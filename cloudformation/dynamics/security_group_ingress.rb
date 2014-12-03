fad3d922f9757f73b8073a90698922faedf63185
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-03 12:04:37 -0500

    Add security_group_ingress dynamic

---
+++ cloudformation/dynamics/security_group_ingress.rb
@@ -0,0 +1,9 @@
+SparkleFormation.dynamic(:security_group_ingress) do |_name, _config={}|
+  resources("#{_name}_security_group_ingress".to_sym) do
+    type 'AWS::EC2::SecurityGroupIngress'
+    from_port _config[:from_port]
+    to_port _config[:to_port]
+    ip_protocol _config[:ip_protocol]
+    source_security_group_name _config[:source_security_group_name]
+  end
+end
