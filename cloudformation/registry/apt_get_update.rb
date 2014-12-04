ab6bd5a6efd72a30d9f750ee98248d0e8b5c6503
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-04 12:09:11 -0500

    Remove do/end from _camel_keys_set

--- cloudformation/registry/apt_get_update.rb
+++ cloudformation/registry/apt_get_update.rb
@@ -1,12 +1,11 @@
 SparkleFormation::Registry.register(:apt_get_update) do
   metadata('AWS::CloudFormation::Init') do
-    _camel_keys_set(:auto_disable) do
-      commands('01_apt_get_update') do
-        command 'sudo apt-get update'
-      end
-      commands('02_apt_get_upgrade') do
-        command 'sudo apt-get upgrade'
-      end
+    _camel_keys_set(:auto_disable)
+    commands('01_apt_get_update') do
+      command 'sudo apt-get update'
+    end
+    commands('02_apt_get_upgrade') do
+      command 'sudo apt-get upgrade'
     end
   end
 end
