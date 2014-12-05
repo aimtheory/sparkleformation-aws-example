cf6c61d7ddd3c235e402e135935e2a6153549abb
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-05 00:18:44 -0500

    Add parameters and set better outputs

--- cloudformation/dynamics/auto_scaling_group.rb
+++ cloudformation/dynamics/auto_scaling_group.rb
@@ -1,40 +1,52 @@
 # Dynamic: auto_scaling_group
 # Description: Creates an AWS::AutoScaling::AutoScalingGroup resource
-# _config inputs: 
+# Parameters:
+#   #{_name}_auto_scaling_group_max_size
+#   #{_name}_auto_scaling_group_min_size
+#
+# Config: 
 #   :launch_configuration_name as ref!
 #   :max_size as integer
 #   :min_size as integer
 #
 # Outputs:
-#    
+#   #{_name}_auto_scaling_group_max_size
+#   #{_name}_auto_scaling_group_min_size 
 
 SparkleFormation.dynamic(:auto_scaling_group) do |_name, _config={}|
 
   asg_name = "#{_name}_auto_scaling_group".to_sym 
+
+  parameters("#{_name}_auto_scaling_group_max_size".to_sym) do
+    type 'Number'
+    description "Max nodes for #{ asg_name }"
+    default 2
+  end
+
+  parameters("#{_name}_auto_scaling_group_min_size".to_sym) do
+    type 'Number'
+    description "Min nodes for #{ asg_name }"
+    default 1
+  end
 
   resources(asg_name) do
     type 'AWS::AutoScaling::AutoScalingGroup'
     properties do
       availability_zones._set('Fn::GetAZs', '')
       launch_configuration_name _config[:launch_configuration_name]
-      max_size _config[:max_size] || 2
-      min_size _config[:min_size] || 1
+      max_size _config[:max_size] || ref!("#{_name}_auto_scaling_group_max_size".to_sym)
+      min_size _config[:min_size] || ref!("#{_name}_auto_scaling_group_max_size".to_sym)
     end
   end
 
-  outputs(asg_name.to_sym) do
-    description "The name of the created #{ ref!(asg_name) } AutoScalingGroup resource"
-    value asg_name
-  end
-
-  outputs("#{_name}_min_size".to_sym) do
-    description 'The min size for the #{ ref!(asg_name) } AutoScalingGroup'
-    value _config[:min_size]
-  end
-
-  outputs("#{_name}_max_size".to_sym) do
-    description 'The max size for the #{ ref!(asg_name) } AutoScalingGroup'
+  outputs("#{_name}_auto_scaling_group_max_size".to_sym) do
+    description "The max size for the #{ asg_name } AutoScalingGroup"
     value _config[:max_size]
+  end
+
+  outputs("#{_name}_auto_scaling_group_min_size".to_sym) do
+    description "The min size for the #{ asg_name } AutoScalingGroup"
+    value _config[:min_size]
   end
 
 end
