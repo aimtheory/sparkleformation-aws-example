f9d90d07e1a72a3e13941e5f5a0be47d91662d2d
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-05 00:30:01 -0500

    Add mysql and nginx install registries

---
+++ cloudformation/registry/mysql_install.rb
@@ -0,0 +1,15 @@
+# Registry: mysql_install
+# Description: A command to install MySQL under Debian as defined in an
+# AWS::AutoScalingGroup::LaunchConfiguration resource. This registry
+# needs to be inserted into such a resource.
+SparkleFormation::Registry.register(:mysql_install) do
+  metadata('AWS::CloudFormation::Init') do
+    _camel_keys_set(:auto_disable)
+    config do
+      commands('mysql_install') do
+        command 'sudo apt-get install mysql_server -y'
+        test 'test ! -d /etc/mysql'
+      end
+    end
+  end
+end
---
+++ cloudformation/registry/nginx_install.rb
@@ -0,0 +1,15 @@
+# Registry: nginx_install
+# Description: A command to install nginx as defined in an
+# AWS::AutoScalingGroup::LaunchConfiguration resource. This registry
+# needs to be inserted into such a resource.
+SparkleFormation::Registry.register(:nginx_install) do
+  metadata('AWS::CloudFormation::Init') do
+    _camel_keys_set(:auto_disable)
+    config do
+      commands('nginx_install') do
+        command 'sudo apt-get install nginx -y'
+        test 'test ! -d /etc/nginx'
+      end
+    end
+  end
+end
