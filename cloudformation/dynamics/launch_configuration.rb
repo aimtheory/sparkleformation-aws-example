477c9d09248d206aaca7698c858a79c4cea2ce58
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-05 00:27:51 -0500

    Add parameters and set better outputs

--- cloudformation/dynamics/launch_configuration.rb
+++ cloudformation/dynamics/launch_configuration.rb
@@ -7,28 +7,49 @@
 #   :key_name as string
 #   :security_groups as array of ref!s
 #
+# Parameters:
+#   image_id as string
+#   instance_type as string
+#   key_name as string
+#   security_groups as comma delimited list
+#
 # Outputs:
 #   #{_name}_launch_configuration as string
 #   image_id as string
-#   instnace_type as string
+#   instance_type as string
 #   key_name as string
-#   security_groups as array of ref!s
+#   security_groups as comma delimited list
 
 SparkleFormation.dynamic(:launch_configuration) do |_name, _config={}|
 
   lc_name = "#{_name}_launch_configuration".to_sym
 
+  parameters("#{_name}_launch_configuration_image_id") do
+    type 'String'
+    description "ImageID for #{ lc_name }"
+    default _config[:image_id] || 'ami-59a4a230'
+  end
+
+  parameters("#{_name}_launch_configuration_instance_type") do
+    type 'String'
+    description "InstanceType for #{ lc_name }"
+    default _config[:instance_type] || 'm1.small'
+  end 
+
+  parameters("#{_name}_launch_configuration_key_name") do
+    type 'String'
+    description "KeyName for #{ lc_name }"
+    default _config[:key_name] || 'sparkleinfrakey'
+  end
+
+  parameters("#{_name}_launch_configuration_security_groups") do
+    type 'CommaDelimitedList'
+    description "SecurityGroups for #{ lc_name }"
+    default _config[:security_groups] || 'default'
+  end
+
   resources(lc_name) do
     type 'AWS::AutoScaling::LaunchConfiguration'
-    metadata('AWS::CloudFormation::Init') do
-      _camel_keys_set(:auto_disable)
-      config do
-        commands('nginx_install') do
-          command 'sudo apt-get install nginx -y'
-          test 'test ! -d /etc/nginx'
-        end
-      end
-    end
     properties do
       image_id _config[:image_id]
       instance_type _config[:instance_type]
@@ -37,28 +58,23 @@ SparkleFormation.dynamic(:launch_configuration) do |_name, _config={}|
     end
   end
 
-  outputs(lc_name) do
-    description "The name of the created #{ lc_name } LaunchConfiguration resource"
-    value "#{_name}_launch_configuration"
-  end
-
-  outputs("#{_name}_image_id".to_sym) do
-    description "The image ID for the #{ lc_name } LaunchConfiguration resource"
+  outputs("#{_name}_launch_configuration_image_id".to_sym) do
+    description "The ImageID for the #{ lc_name } LaunchConfiguration resource"
     value _config[:image_id]
   end
 
-  outputs("#{_name}_instance_type".to_sym) do
-    description "The instance type for the #{ lc_name } LaunchConfiguration resource"
+  outputs("#{_name}_launch_configuration_instance_type".to_sym) do
+    description "The InstanceType for the #{ lc_name } LaunchConfiguration resource"
     value _config[:instance_type]
   end
 
-  outputs("#{_name}_key_name".to_sym) do
-    description 'The key pair name for the #{ lc_name } LaunchConfiguration resource'
+  outputs("#{_name}_launch_configuration_key_name".to_sym) do
+    description 'The KeyName for the #{ lc_name } LaunchConfiguration resource'
     value _config[:key_name]
   end
 
-  outputs("#{_name}_security_groups".to_sym) do
-    description "The list of security groups for the #{ lc_name } LaunchConfiguration resource"
+  outputs("#{_name}_launch_configuration_security_groups".to_sym) do
+    description "The SecurityGroups for the #{ lc_name } LaunchConfiguration resource"
     value _config[:security_groups].join(',')
   end
 
