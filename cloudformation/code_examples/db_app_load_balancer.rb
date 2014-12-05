60536dec1b076b90efd87835dbebd7f161dbd952
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-05 00:16:42 -0500

    Use defaults in load_balancer dynamic, don't override

--- cloudformation/code_examples/db_app_load_balancer.rb
+++ cloudformation/code_examples/db_app_load_balancer.rb
@@ -1,16 +1,8 @@
 SparkleFormation.new('db_app_load_balancer').load(:base).overrides do
 
   description 'Database application load balancer'
-
-  # Setup reusable values
-  app_protocol = 'tcp'
-  app_port = '80'
  
-  # Create the load balancer resource
-  dynamic!(:load_balancer, 'db_app',
-           :protocol => app_protocol,
-           :instance_port => app_port,
-           :instance_protocol => app_protocol
-  )
+  # Create the load balancer resource and use defaults
+  dynamic!(:load_balancer, 'db_app')
 
 end
