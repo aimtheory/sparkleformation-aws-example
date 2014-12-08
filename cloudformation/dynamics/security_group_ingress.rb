000614628ebf463faa683c8bb1091ef6ecdfc42b
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-08 13:18:50 -0500

    Fixup some sg property values

--- cloudformation/dynamics/security_group_ingress.rb
+++ cloudformation/dynamics/security_group_ingress.rb
@@ -1,22 +1,22 @@
 # Dynamic: security_group_ingress
 # Description: Creates an AWS::EC2::SecurityGroupIngress rule resource
 # Config:
-#   :from_port as integer
-#   :to_port as integer
+#   :from_port as number
+#   :to_port as number
 #   :ip_protocol as string
 #   :group_name as string
 #   :source_security_group_name as string
 #
 # Parameters:
-#   #{_name}_security_group_ingress_from_port as integer
-#   #{_name}_security_group_ingress_to_port as integer
+#   #{_name}_security_group_ingress_from_port as number
+#   #{_name}_security_group_ingress_to_port as number
 #   #{_name}_security_group_ingress_ip_protocol as string
 #   #{_name}_security_group_ingress_group_name as string
 #   #{_name}_security_group_ingress_source_security_group_name as string
 #
 # Outputs: (as ref!s to the parameters' user defined values)
-#   #{_name}_security_group_ingress_from_port as integer
-#   #{_name}_security_group_ingress_to_port as integer
+#   #{_name}_security_group_ingress_from_port as number
+#   #{_name}_security_group_ingress_to_port as number
 #   #{_name}_security_group_ingress_ip_protocol as string
 #   #{_name}_security_group_ingress_group_name as string
 #   #{_name}_security_group_ingress_source_security_group_name as
@@ -24,13 +24,13 @@
 SparkleFormation.dynamic(:security_group_ingress) do |_name, _config={}|
   
   parameters("#{_name}_security_group_ingress_from_port".to_sym) do
-    type 'Integer'
+    type 'Number'
     description "The FromPort for the #{_name} SecurityGroupIngress rule"
     default _config[:from_port] || '22'
   end
 
   parameters("#{_name}_security_group_ingress_to_port".to_sym) do
-    type 'Integer'
+    type 'Number'
     description "The ToPort for the #{_name} SecurityGroupIngress rule"
     default _config[:to_port] || '22'
   end
@@ -41,26 +41,14 @@ SparkleFormation.dynamic(:security_group_ingress) do |_name, _config={}|
     default _config[:ip_protocol] || 'tcp'
   end
 
-  parameters("#{_name}_security_group_ingress_group_name".to_sym) do
-    type 'String'
-    description "The GroupName for the #{_name} SecurityGroupIngress rule"
-    default _config[:group_name] || 'default'
-  end
-
-  parameters("#{_name}_security_group_ingress_source_security_group_name".to_sym) do
-    type 'String'
-    description "The SourceSecurityGroupName for the #{_name} SecurityGroupIngress rule"
-    default _config[:source_security_group_name] || 'default'
-  end
-
   resources("#{_name}_security_group_ingress".to_sym) do
     type 'AWS::EC2::SecurityGroupIngress'
     properties do
       from_port ref!("#{_name}_security_group_ingress_from_port".to_sym)
       to_port ref!("#{_name}_security_group_ingress_to_port".to_sym)
       ip_protocol ref!("#{_name}_security_group_ingress_ip_protocol".to_sym)
-      group_name ref!("#{_name}_security_group_ingress_group_name".to_sym)
-      source_security_group_name ref!("#{_name}_security_group_ingress_source_security_group_name".to_sym)
+      group_name _config[:group_name]
+      source_security_group_name _config[:source_security_group_name]
     end
   end
 
@@ -81,12 +69,12 @@ SparkleFormation.dynamic(:security_group_ingress) do |_name, _config={}|
 
   outputs("#{_name}_security_group_ingress_group_name".to_sym) do
     description "The GroupName for the #{_name} SecurityGroupIngress rule"
-    value ref!("#{_name}_security_group_ingress_group_name".to_sym)
+    value _config[:group_name]
   end
 
   outputs("#{_name}_security_group_ingress_source_security_group_name".to_sym) do
     description "The SourceSecurityGroupName for the #{_name} SecurityGroupIngress rule"
-    value ref!("#{_name}_security_group_ingress_source_security_group_name".to_sym)
+    value _config[:source_security_group_name]
   end
 
 end
