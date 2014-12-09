f3858d661b9f8b8b8720cee4496dafa244b60fb0
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-09 11:09:18 -0500

    Cosolidate min_size, max_size and desired_capacity parameters to just 'size'

--- cloudformation/dynamics/auto_scaling_group.rb
+++ cloudformation/dynamics/auto_scaling_group.rb
@@ -1,38 +1,23 @@
 # Dynamic: auto_scaling_group
 # Description: Creates an AWS::AutoScaling::AutoScalingGroup resource
 # Parameters:
-#   #{_name}_auto_scaling_group_max_size
-#   #{_name}_auto_scaling_group_min_size
+#   #{_name}_auto_scaling_size
 #
 # Config: 
 #   :launch_configuration_name as ref!
-#   :max_size as number
-#   :min_size as number
+#   :size as number
 #
 # Outputs:
-#   #{_name}_auto_scaling_group_max_size
-#   #{_name}_auto_scaling_group_min_size 
+#   #{_name}_auto_scaling_group_size
 
 SparkleFormation.dynamic(:auto_scaling_group) do |_name, _config={}|
 
   asg_name = "#{_name}_auto_scaling_group".to_sym 
 
-  parameters("#{_name}_auto_scaling_group_max_size".to_sym) do
+  parameters("#{_name}_auto_scaling_group_size".to_sym) do
     type 'Number'
-    description "Max nodes for #{ asg_name }"
-    default 2
-  end
-
-  parameters("#{_name}_auto_scaling_group_min_size".to_sym) do
-    type 'Number'
-    description "Min nodes for #{ asg_name }"
-    default 1
-  end
-
-  parameters("#{_name}_auto_scaling_group_desired_capacity".to_sym) do
-    type 'Number'
-    description "Desired capacity for the the AutoScalingGroup"
-    default 2
+    description "Number of desired nodes for #{ asg_name }"
+    default _config[:size]
   end
 
   # Add a load balancer if one is specified
@@ -49,9 +34,9 @@ SparkleFormation.dynamic(:auto_scaling_group) do |_name, _config={}|
     properties do
       availability_zones._set('Fn::GetAZs', '')
       launch_configuration_name _config[:launch_configuration_name]
-      max_size _config[:max_size] || ref!("#{_name}_auto_scaling_group_max_size".to_sym)
-      min_size _config[:min_size] || ref!("#{_name}_auto_scaling_group_max_size".to_sym)
-      desired_capacity _config[:desired_capacity] || ref!("#{_name}_auto_scaling_group_desired_capacity".to_sym)
+      max_size ref!("#{_name}_auto_scaling_group_size".to_sym)
+      min_size ref!("#{_name}_auto_scaling_group_size".to_sym)
+      desired_capacity ref!("#{_name}_auto_scaling_group_size".to_sym)
       load_balancer_names _config[:load_balancer_names] if _config[:load_balancer_names]
     end
     creation_policy do
@@ -63,13 +48,17 @@ SparkleFormation.dynamic(:auto_scaling_group) do |_name, _config={}|
   end
 
   outputs("#{_name}_auto_scaling_group_max_size".to_sym) do
-    description "The max size for the #{ asg_name } AutoScalingGroup"
-    value ref!("#{_name}_auto_scaling_group_max_size".to_sym)
+    description "The MaxSize for the #{ asg_name } AutoScalingGroup"
+    value ref!("#{_name}_auto_scaling_group_size".to_sym)
   end
 
   outputs("#{_name}_auto_scaling_group_min_size".to_sym) do
-    description "The min size for the #{ asg_name } AutoScalingGroup"
-    value ref!("#{_name}_auto_scaling_group_max_size".to_sym)
+    description "The MinSize for the #{ asg_name } AutoScalingGroup"
+    value ref!("#{_name}_auto_scaling_group_size".to_sym)
   end
 
+  outputs("#{_name}_auto_scaling_group_desired_capacity".to_sym) do
+    description "The DesiredCapacity for the #{ asg_name } AutoScalingGroup"
+    value ref!("#{_name}_auto_scaling_group_size".to_sym)
+  end
 end
