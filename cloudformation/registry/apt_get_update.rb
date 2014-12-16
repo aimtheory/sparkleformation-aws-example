bb9956669db26ce76c39a0df6bf9dcd72004fa39
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-15 19:38:08 -0500

    Add missing config block

--- cloudformation/registry/apt_get_update.rb
+++ cloudformation/registry/apt_get_update.rb
@@ -1,11 +1,15 @@
 SparkleFormation::Registry.register(:apt_get_update) do
   metadata('AWS::CloudFormation::Init') do
     _camel_keys_set(:auto_disable)
-    commands('01_apt_get_update') do
-      command 'sudo apt-get update'
-    end
-    commands('02_apt_get_upgrade') do
-      command 'sudo apt-get upgrade'
+    config do
+      commands('01_apt_get_update') do
+        command 'sudo apt-get update'
+        test 'test -z ` HISTCONTROL=ignorespace history | grep "apt-get update"`'
+      end
+      commands('02_apt_get_upgrade') do
+        command 'sudo apt-get upgrade -y'
+        test 'test -z ` HISTCONTROL=ignorespace history | grep "apt-get upgrade"`'
+      end
     end
   end
 end
