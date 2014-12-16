fd7c0e746affc5cf861cd9cf66275c681e99f102
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-12-15 20:12:40 -0500

    Better mysql installation

--- cloudformation/registry/mysql_install.rb
+++ cloudformation/registry/mysql_install.rb
@@ -7,7 +7,7 @@ SparkleFormation::Registry.register(:mysql_install) do
     _camel_keys_set(:auto_disable)
     config do
       commands('mysql_install') do
-        command 'debconf-set-selections <<< "mysql-server mysql-server/root_password password password1" && debconf-set-selections <<< "mysql-server mysql-server/root_password_again password password1" && apt-get -y install mysql-server && service mysql start'
+        command 'DEBIAN_FRONTEND=noninteractive apt-get -q -y install mysql-server && service mysql start'
         test 'test ! -d /etc/mysql'
       end
     end
